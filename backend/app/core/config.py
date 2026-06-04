53d3ffc8858752e748b00f3e677713b415bd7169
vikdov <mr.dovgoshiya@gmail.com>
2026-06-04 18:41:46 +0200

    cd: cold release timing run

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -196,7 +196,7 @@ class Settings(BaseSettings):
     # =========================================================================
 
     API_TITLE: str = "LingvoPal"
-    API_VERSION: str = "1.0.3"
+    API_VERSION: str = "1.0.4"
     API_HOST: str = "0.0.0.0"  # nosec B104
     API_PORT: int = 8000
 
